1e67b454597bc19304e21efa9b4ac4eb5e56e74e
omkar shelke <shelkeom230@gmail.com>
2025-08-19 12:06:35 +0530

    resolved noreferrer

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -35,7 +35,7 @@ export const Socialicons = (params) => {
           const IconComponent = ICON_MAPPING[platform] || ICON_MAPPING.default;
           return (
             <li key={platform}>
-              <a href={url} target="_blank" rel="nonreferrer">
+              <a href={url} target="_blank" rel="noreferrer">
                 <IconComponent />
               </a>
             </li>
